dd7debbcc68ddf3fd99a2435878d28b4c1f82fcf
Pc3xX <115648301+Pc3xX@users.noreply.github.com>
2023-02-23 18:09:08 +0300

    Add files via upload

--- PDD_bot.py
+++ PDD_bot.py
@@ -43,21 +43,71 @@ async def menu(message: types.Message):
     if message.text == '10 билетов':
         await User.bilet10.set()
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        buttons = ["Нет конечно", "Может быть"]
+        buttons = ['1️⃣',
+                   "2️⃣",
+                   "3️⃣",
+                   'Выйти']
         keyboard.add(*buttons)
-        await message.answer("Правильно он поступил?", reply_markup=keyboard)
+        await message.answer("В каком случае Вы совершите вынужденную остановку?\n"
+                             "\n"
+                             "Варианты ответа:\n"
+                             "\n"
+                             " 1. Остановившись непосредственно перед пешеходным переходом, чтобы уступить "
+                             " дорогу пешеходу.\n"
+                             " 2. Остановившись на проезжей части из-за технической неисправности транспортного средства.\n"
+                             " 3. В обоих перечисленных случаях", reply_markup=keyboard)
     if message.text == '20 билетов':
         await User.bilet20.set()
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        buttons = ["Да", "Нет"]
+
+        photo = open('1.jpg', 'rb')
+
+        buttons = ['1️⃣',
+                   "2️⃣",
+                   "3️⃣",
+                   'Выйти']
         keyboard.add(*buttons)
-        await message.answer("Стоит ли давать взятки?", reply_markup=keyboard)
+        await message.answer_photo(photo, caption=
+                            "Разрешен ли Вам съезд на дорогу с грунтовым покрытием?\n"
+                             "\n"
+                             "Варианты ответа:\n"
+                             "\n"
+                             " 1. Разрешен.\n "
+                             " 2. Разрешен только при технической неисправности транспортного средства.\n"
+                             " 3. Запрещен.", reply_markup=keyboard)
     if message.text == '30 билетов':
         await User.bilet30.set()
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         buttons = ["Нарушает закон", "Превышает скорость"]
         keyboard.add(*buttons)
         await message.answer("Что он делает?", reply_markup=keyboard)
+
+@dp.message_handler(state='bilet10')
+async def anwser_10_1(message: types.Message):
+    global connection
+    if message.text == "2️⃣":
+        await User.bilet10.set()
+
+        photo = open('2.jpg','rb')
+
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        buttons = ['1️⃣',
+                   "2️⃣",
+                   "3️⃣",
+                   '/Выйти']
+        keyboard.add(*buttons)
+        await message.answer_photo( photo, caption = "Можно ли Вам остановиться в указанном месте для посадки пассажира?\n"
+                             "\n"
+                             "Варианты ответа:\n"
+                             "\n"
+                             " 1. Можно.\n"
+                             " 2. Можно, если Вы управляете такси.\n"
+                             " 3. Нельзя", reply_markup=keyboard)
+    if message.text == '1️⃣' or "3️⃣":
+        await message.answer('❌Подумай еще!❌')
+
+
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
